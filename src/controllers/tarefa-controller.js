76bbaf68086a375be62d719f891ac06ceaac32fe
brunosa91 <brunosantana.engenharia@gmail.com>
2022-02-24 18:48:01 -0300

    atualização api

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -1,6 +1,14 @@
 const tarefaController = (app) => {
   app.get("/tarefa", (req, res) => {
     res.send("rota GET para entidade tarefa");
+    res.json({
+      Tarefas: [],
+    });
+  });
+  app.post("/tarefa", (req, res) => {
+    res.json({
+      msg: "Tarefa inserida com sucesso",
+    });
   });
 };
 
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,6 +1,15 @@
 const usuarioController = (app) => {
   app.get("/usuario", (req, res) => {
     res.send("rota GET para entidade usuário");
+    res.json({
+      usuarios: [],
+    });
+  });
+
+  app.post("/usuario", (req, res) => {
+    res.json({
+      msg: "Tarefa inserida com sucesso",
+    });
   });
 };
 
